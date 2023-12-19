c8eb83f03b4b5aa979eb9aa5567ef4ff8ca22908
arjunv075 <arjunv075@gmail.com>
2023-12-19 18:32:59 +0530

    add App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import Home from './components/home/home'
+import Employee from './components/employee/employee';
+import About from './components/about/about'
+import Header from './components/header/header'
+import EmployeeDetails from './components/employeedetails/employeedetails';
+import LeaveForm from './components/leaves/leaves';
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element = {<About />}/>
+        <Route path="/employees" element={<Employee />}/>
+        <Route path="/employees/:id" element={<EmployeeDetails />}>
+          <Route path="leave" element={<LeaveForm />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
